99c11f601359e9f21e5bec667f1b191dbac2fa52
vineethsubbaraya <vineeth.subbaraya@gmail.com>
2020-10-02 16:34:17 -0400

    Add tox and update readme

--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 A simple library in Python to convert text to 6-dots pattern braille (Grade 1)
 
 ## Installation
+
+Run the following to install the package
+
+```python
+pip install pybraille
+```
+
+## Usage
+
+```python
+from pybraille import convertText
+
+print(convertText("hello"))
+>>> ⠓⠑⠇⠇⠕
+
+print(convertText("filename.txt")) #eg: tests/sample.txt
+>>> ⠠⠞⠓⠊⠎ ⠙⠊⠗⠑⠉⠞⠕⠗⠽ ⠉⠕⠝⠞⠁⠊⠝⠎ ⠞⠑⠎⠞ ⠋⠊⠇⠑⠎⠲
+```
+
+## Testing
+
+This library needs is tested against python 3.6 and 3.7. Both interpreters need to be available to tox
+
+Running tests:
+
+```tox``` or ```pytest```
+
+#### Support
+Any python 3.x version should work.
--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
 from setuptools import setup, find_packages
- 
-classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: OS Independent',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3',
-  'Programming Language :: Python :: 3.5',
-  'Programming Language :: Python :: 3.6',
-  'Programming Language :: Python :: 3.7',
-  'Programming Language :: Python :: 3.8',
-]
 
 with open("README.md", "r") as rd:
   long_description = rd.read()
@@ -20,12 +8,21 @@ setup(
   version='0.0.1',
   description='A library to conver text to 6-dot braille pattern(Grade 1)',
   long_description=long_description,
-  long_description_content_type="text/markdown",
   url='https://github.com/vineethsubbaraya/pybraille',  
   author='Vineeth Subbaraya',
   author_email='vsubbaraya033@gmail.com',
   license='MIT', 
-  classifiers=classifiers,
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
   keywords=['python', 'braille', 'text to braille'],
   packages=find_packages(),
   install_requires=['']
